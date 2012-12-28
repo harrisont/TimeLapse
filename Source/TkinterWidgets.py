7fdf5f543a5184af23071454fc41cdeb2d27405d
Harrison Ting <runhappy@gmail.com>
2012-12-28 16:23:27 -0500

    Added LabelledEntryControl and WidthAndHeightControl.

--- Source/TkinterWidgets.py
+++ Source/TkinterWidgets.py
@@ -40,5 +40,57 @@ class IntegerEntry(tk.Entry):
 		except ValueError:
 			return False
 
+	def IsValid(self):
+		return IntegerEntry._IsValidStatic(self.get())
+
+class LabelledEntryControl(tk.Frame):
+	"""A frame with a label and an entry to the right of it."""
+
+	def __init__(self, parent, labelText, **keywordArgs):
+		"""Uses Tkinter.Entry by default.  Pass in a value for 'entryClass' to use a differnt entry class."""
+
+		entryClass = tk.Entry
+		if 'entryClass' in keywordArgs:
+			entryClass = keywordArgs['entryClass']
+			del keywordArgs['entryClass']
+
+		tk.Frame.__init__(self, parent, keywordArgs)
+
+		tk.Label(self, text=labelText).pack(side=tk.LEFT)
+
+		self.entry = entryClass(self)
+		self.entry.pack()
+
+	def GetText(self):
+		return self.entry.get()
+
+	def IsEmpty(self):
+		return len(self.GetText()) == 0
+
+class WidthAndHeightControl(tk.Frame):
+	"""A frame with two LabelledEntryControl's for width and height."""
+
+	def __init__(self, parent, **keywordArgs):
+		tk.Frame.__init__(self, parent, keywordArgs)
+
+		self.widthControl = LabelledEntryControl(self, 'Width', entryClass=IntegerEntry)
+		self.widthControl.pack()
+
+		self.heightControl = LabelledEntryControl(self, 'Height', entryClass=IntegerEntry)
+		self.heightControl.pack()
+
+	def GetWidth(self):
+		return self.widthControl.GetText()
+
+	def GetHeight(self):
+		return self.heightControl.GetText()
+
+	def IsValid(self):
+		bothEmpty = self.widthControl.IsEmpty() and self.heightControl.IsEmpty()
+		bothFilled = not self.widthControl.IsEmpty() and not self.heightControl.IsEmpty()
+
+		return self.widthControl.entry.IsValid() \
+			and (bothEmpty or bothFilled)
+
 if __name__=='__main__':
 	doctest.testmod()
